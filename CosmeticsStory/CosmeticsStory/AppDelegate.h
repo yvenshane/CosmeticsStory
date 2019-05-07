b6cd84ab71bccfd655ccfdb0ec545ce9d008cd8c
YVEN SHANE <yvenshane@aol.com>
2019-05-07 11:43:17 +0800

    Create project

---
+++ CosmeticsStory/CosmeticsStory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CosmeticsStory
+//
+//  Created by YVEN on 2019/5/7.
+//  Copyright © 2019 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CosmeticsStory/CosmeticsStory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CosmeticsStory
+//
+//  Created by YVEN on 2019/5/7.
+//  Copyright © 2019 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
